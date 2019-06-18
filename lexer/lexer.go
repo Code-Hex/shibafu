a90ac1f141359cb09b7bc641d68672c858d929c8
Kei Kamikawa <x00.x7f@gmail.com>
2019-06-18 18:33:19 +0900

    Added lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -12,51 +12,96 @@ type Lexer struct {
 	position     int
 	readPosition int
 	ch           rune
+	col          int
 	line         int
 }
 
 // New initializes a new lexer with input string
 func New(input string) *Lexer {
-	l := &Lexer{input: []rune(input)}
+	l := &Lexer{
+		input: []rune(input),
+		line:  1,
+	}
 	l.readChar()
 	return l
 }
 
-func (l *Lexer) NextToken() token.Token {
+func (l *Lexer) NextToken() *token.Token {
 	l.skipComments()
-	l.readChar()
-	for {
-		switch {
-		case strings.ContainsRune(token.INCR, l.ch),
-			strings.ContainsRune(token.DECR, l.ch),
-			strings.ContainsRune(token.NEXT, l.ch),
-			strings.ContainsRune(token.PREV, l.ch),
-			strings.ContainsRune(token.READ, l.ch),
-			strings.ContainsRune(token.WRITE, l.ch),
-			strings.ContainsRune(token.OPEN, l.ch),
-			strings.ContainsRune(token.CLOSE, l.ch):
-			continue
-		}
-	}
-	return token.Token{}
+	return l.readIdents()
 }
 
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
-		// ascii code's null
+		// ascii code's null (EOF)
 		l.ch = 0
 	} else {
 		l.ch = l.input[l.readPosition]
 	}
 	l.position = l.readPosition
 	l.readPosition++
+	l.col++
 }
 
 func (l *Lexer) skipComments() {
-	for l.ch != 'w' || l.ch != 'W' {
+	for l.ch != 'w' && l.ch != 'W' {
 		if l.ch == '\n' {
+			l.col = 0
 			l.line++
 		}
 		l.readChar()
+	}
+}
+
+func (l *Lexer) readIdents() *token.Token {
+	position := l.position
+	defer l.readChar()
+
+	for l.ch == 'w' || l.ch == 'W' {
+		input := string(l.input[position:l.readPosition])
+
+		switch input {
+		case token.INCR:
+			return l.newToken(token.INCR)
+		case token.DECR:
+			return l.newToken(token.DECR)
+		case token.NEXT:
+			return l.newToken(token.NEXT)
+		case token.PREV:
+			return l.newToken(token.PREV)
+		case token.READ:
+			return l.newToken(token.READ)
+		case token.WRITE:
+			return l.newToken(token.WRITE)
+		case token.OPEN:
+			return l.newToken(token.OPEN)
+		case token.CLOSE:
+			return l.newToken(token.CLOSE)
+		}
+
+		switch {
+		case strings.HasPrefix(token.INCR, input),
+			strings.HasPrefix(token.DECR, input),
+			strings.HasPrefix(token.NEXT, input),
+			strings.HasPrefix(token.PREV, input),
+			strings.HasPrefix(token.READ, input),
+			strings.HasPrefix(token.WRITE, input),
+			strings.HasPrefix(token.OPEN, input),
+			strings.HasPrefix(token.CLOSE, input):
+			l.readChar()
+			continue
+		}
+
+		break
+	}
+	return l.newToken(token.ILLEGAL)
+}
+
+func (l *Lexer) newToken(typ token.Type) *token.Token {
+	return &token.Token{
+		Type:    typ,
+		Literal: string(typ),
+		Col:     l.col,
+		Line:    l.line,
 	}
 }
---
+++ lexer/lexer_test.go
@@ -0,0 +1,114 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/Code-Hex/shibafu/token"
+)
+
+func TestNextToken(t *testing.T) {
+	type want struct {
+		Type    token.Type
+		Literal string
+		Col     int
+		Line    int
+	}
+
+	tests := []struct {
+		name  string
+		input string
+		wants []want
+	}{
+		{
+			name:  "Valid joined",
+			input: `wwwwwwwwwWWWWWWwWwwWwWwWWwWwwWWwwWWWWwwWWwwwwWwwwwWwwWWwWWWWwWW`,
+			wants: []want{
+				{token.INCR, token.INCR, 3, 1},
+				{token.INCR, token.INCR, 6, 1},
+				{token.INCR, token.INCR, 9, 1},
+				{token.DECR, token.DECR, 12, 1},
+				{token.DECR, token.DECR, 15, 1},
+				{token.NEXT, token.NEXT, 18, 1},
+				{token.NEXT, token.NEXT, 21, 1},
+				{token.PREV, token.PREV, 24, 1},
+				{token.PREV, token.PREV, 27, 1},
+				{token.READ, token.READ, 31, 1},
+				{token.READ, token.READ, 35, 1},
+				{token.WRITE, token.WRITE, 39, 1},
+				{token.WRITE, token.WRITE, 43, 1},
+				{token.OPEN, token.OPEN, 48, 1},
+				{token.OPEN, token.OPEN, 53, 1},
+				{token.CLOSE, token.CLOSE, 58, 1},
+				{token.CLOSE, token.CLOSE, 63, 1},
+			},
+		},
+		{
+			name:  "Valid spaced",
+			input: `www www www WWW WWW wWw wWw WwW WwW wwWW wwWW WWww WWww wwWww wwWww WWwWW WWwWW`,
+			wants: []want{
+				{token.INCR, token.INCR, 3, 1},
+				{token.INCR, token.INCR, 7, 1},
+				{token.INCR, token.INCR, 11, 1},
+				{token.DECR, token.DECR, 15, 1},
+				{token.DECR, token.DECR, 19, 1},
+				{token.NEXT, token.NEXT, 23, 1},
+				{token.NEXT, token.NEXT, 27, 1},
+				{token.PREV, token.PREV, 31, 1},
+				{token.PREV, token.PREV, 35, 1},
+				{token.READ, token.READ, 40, 1},
+				{token.READ, token.READ, 45, 1},
+				{token.WRITE, token.WRITE, 50, 1},
+				{token.WRITE, token.WRITE, 55, 1},
+				{token.OPEN, token.OPEN, 61, 1},
+				{token.OPEN, token.OPEN, 67, 1},
+				{token.CLOSE, token.CLOSE, 73, 1},
+				{token.CLOSE, token.CLOSE, 79, 1},
+			},
+		},
+		{
+			name: "Valid spaced multi lines",
+			input: `www www www WWW WWW wWw 
+wWw WwW WwW wwWW wwWW WWww WWww wwWww
+wwWww WWwWW WWwWW`,
+			wants: []want{
+				{token.INCR, token.INCR, 3, 1},
+				{token.INCR, token.INCR, 7, 1},
+				{token.INCR, token.INCR, 11, 1},
+				{token.DECR, token.DECR, 15, 1},
+				{token.DECR, token.DECR, 19, 1},
+				{token.NEXT, token.NEXT, 23, 1},
+				{token.NEXT, token.NEXT, 3, 2},
+				{token.PREV, token.PREV, 7, 2},
+				{token.PREV, token.PREV, 11, 2},
+				{token.READ, token.READ, 16, 2},
+				{token.READ, token.READ, 21, 2},
+				{token.WRITE, token.WRITE, 26, 2},
+				{token.WRITE, token.WRITE, 31, 2},
+				{token.OPEN, token.OPEN, 37, 2},
+				{token.OPEN, token.OPEN, 5, 3},
+				{token.CLOSE, token.CLOSE, 11, 3},
+				{token.CLOSE, token.CLOSE, 17, 3},
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			l := New(tc.input)
+			for i, want := range tc.wants {
+				tok := l.NextToken()
+				if tok.Type != want.Type {
+					t.Errorf("tests[%d] - tokentype wrong. want=%q, got=%q", i, want.Type, tok.Type)
+				}
+				if tok.Literal != want.Literal {
+					t.Errorf("tests[%d] - literal wrong. want=%q, got=%q", i, want.Literal, tok.Literal)
+				}
+				if tok.Col != want.Col {
+					t.Errorf("tests[%d] - column wrong. want=%d, got=%d", i, want.Col, tok.Col)
+				}
+				if tok.Line != want.Line {
+					t.Errorf("tests[%d] - line number wrong. want=%d, got=%d", i, want.Line, tok.Line)
+				}
+			}
+		})
+	}
+}
