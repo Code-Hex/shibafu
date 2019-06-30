13248c1c40ef3c149e61a8f17db29fa93336ac45
Kei Kamikawa <x00.x7f@gmail.com>
2019-06-30 12:19:17 +0900

    Added compile error

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -97,7 +97,7 @@ LOOP:
 			jmpStack = append(jmpStack, pc)
 		case token.CLOSE:
 			if len(jmpStack) == 0 {
-				return errors.New("compile error")
+				return runtimeError(t, "undefined `wWW`")
 			}
 			jmpLabel := jmpStack[len(jmpStack)-1]
 			jmpStack = jmpStack[:len(jmpStack)-1]
@@ -107,7 +107,7 @@ LOOP:
 			})
 			e.program[jmpLabel].pc = pc
 		case token.ILLEGAL:
-			return errors.New("compile illegal error")
+			return runtimeError(t, "compile illegal token")
 		}
 	}
 	return nil
@@ -153,4 +153,8 @@ func (e *Evaluator) execute(ctx context.Context) error {
 		}
 	}
 	return nil
+}
+
+func runtimeError(t *token.Token, msg string) error {
+	return fmt.Errorf("runtime:%d:%d: %s", t.Line, t.Col, msg)
 }
